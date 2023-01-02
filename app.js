ec7491f3341a5b5abaa385c65cec3c68de1ff1b7
kaan <kaanayten@hotmail.com>
2023-01-02 22:49:57 +0300

    login page updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
@@ -17,6 +18,10 @@ mongoose.connect("mongodb://localhost/smartedu-db").then(() => {
 // TEMPLATE ENGINE
 app.set("view engine", "ejs");
 
+// GLOBAL VARIABLE
+
+global.userIn = null;
+
 // MIDDLEWARES
 app.use(express.static("public"));
 app.use(express.json()); // for parsing application/json
@@ -26,10 +31,15 @@ app.use(
     secret: "my_keyboard_cat",
     resave: false,
     saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: "mongodb://localhost/smartedu-db" }),
   })
 );
 
 // ROUTES
+app.use("*", (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use("/", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
