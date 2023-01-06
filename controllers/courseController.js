d7514e7718e5358583b867d5aedbfba841a2d523
kaan <kaanayten@hotmail.com>
2023-01-07 02:39:00 +0300

    course-page-delete-update-button-added

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -11,12 +11,11 @@ exports.createCourse = async (req, res) => {
       user: req.session.userID,
     });
 
+    req.flash("success", `${course.name} Has been created successfully`);
     res.status(201).redirect("/courses");
   } catch (error) {
-    res.status(400).json({
-      status: "fail",
-      error,
-    });
+    req.flash("error", `Something happened!`);
+    res.status(400).redirect("/courses");
   }
 };
 
@@ -108,6 +107,19 @@ exports.releaseCourse = async (req, res) => {
     await user.courses.pull({ _id: req.body.course_id });
     await user.save();
 
+    res.status(200).redirect("/users/dashboard");
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
+
+exports.deleteCourse = async (req, res) => {
+  try {
+    const course = await Course.findOneAndRemove({ slug: req.params.slug });
+    req.flash("error", `${course.name} Has been removed successfully`);
     res.status(200).redirect("/users/dashboard");
   } catch (error) {
     res.status(400).json({
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -32,7 +32,8 @@ exports.getContactPage = (req, res) => {
 };
 
 exports.sendeEmail = async (req, res) => {
-  const outputMessage = `
+  try {
+    const outputMessage = `
   
   <h1>Mail Details</h1>
   <ul>
@@ -43,31 +44,38 @@ exports.sendeEmail = async (req, res) => {
     <p>${req.body.message}</p>
   `;
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: "kaanayten67@gmail.com", // gmail account
-      pass: "zsafusxafrpqicbu", // gmail password
-    },
-  });
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: "kaanayten67@gmail.com", // gmail account
+        pass: "zsafusxafrpqicbu", // gmail password
+      },
+    });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Smart Edu Contact Form" <kaanayten67@gmail.com>', // sender address
-    to: "kaanayten@hotmail.com", // list of receivers
-    subject: "Smart Edu Contact Form New Message ✔", // Subject line
-    html: outputMessage, // html body
-  });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Smart Edu Contact Form" <kaanayten67@gmail.com>', // sender address
+      to: "kaanayten@hotmail.com", // list of receivers
+      subject: "Smart Edu Contact Form New Message ✔", // Subject line
+      html: outputMessage, // html body
+    });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-  res.status(200).redirect("/contact");
+    req.flash("success", "We Received your message succesfully");
+
+    res.status(200).redirect("/contact");
+  } catch (err) {
+    // req.flash("error", `Something happened! ${err}`);
+    req.flash("error", `Something happened!`);
+    res.status(200).redirect("/contact");
+  }
 };
